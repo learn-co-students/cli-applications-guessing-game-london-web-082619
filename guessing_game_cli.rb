758c3725dc6edcd6c45856da6fb0509991efea4f
github username <github email address>
2019-09-10 11:07:41 +0100

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   input = gets.chomp
 
-  if input == random.to_s
+  if input == random
     puts "You guessed the correct number!"
   elsif input.downcase == "exit"
     puts "Goodbye!"
